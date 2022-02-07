ea183dbe7de89ca4722426c508bfd9633101815b
Shane McCarthy <mccartsf@dukes.jmu.edu>
2022-02-06 20:03:09 -0500

    Changes to the Master Site adding more selections from top

--- Student.aspx.designer.cs
+++ Student.aspx.designer.cs
@@ -33,12 +33,12 @@ namespace Lab1
         protected global::System.Web.UI.WebControls.TextBox SearchStudentID;
 
         /// <summary>
-        /// Table1 control.
+        /// StudentNames control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Table Table1;
+        protected global::System.Web.UI.WebControls.Table StudentNames;
     }
 }
