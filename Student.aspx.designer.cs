5149740ae6bc8fd0a82dd1139cc68621e5ceee0e
Timothy Goeke Dee <tgdee123@gmail.com>
2022-02-07 16:52:22 -0500

    Connection String in student

--- Student.aspx.designer.cs
+++ Student.aspx.designer.cs
@@ -166,5 +166,14 @@ namespace Lab1
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button Button2;
+
+        /// <summary>
+        /// allStudentData control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource allStudentData;
     }
 }
