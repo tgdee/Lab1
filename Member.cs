4893bc00a159654fd91fe13bfde12999bd0b9bd6
Shane McCarthy <mccartsf@dukes.jmu.edu>
2022-02-07 20:59:02 -0500

    changes made to the member csharp constructor

---
+++ Member.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab1
+{
+    public class Member
+    {
+        public String fName { get; private set; }
+        public String lName { get; private set; }
+        public String email { get; private set; }
+
+    public Member(String fName, String lName, String email)
+        {
+            this.fName = fName;
+            this.lName = lName;
+            this.email = email;
+
+        }
+
+        public override string ToString()
+        {
+            String description = "";
+            description += this.fName + "\t" + this.lName + "\t" + this.email;
+
+            return description;
+        }
+    }
+
+}
