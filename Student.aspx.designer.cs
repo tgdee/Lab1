767e593649b29abf5cf19f7ba25709fe3efb0ca8
Timothy Goeke Dee <tgdee123@gmail.com>
2022-02-09 14:16:41 -0500

    Formatted gridview

--- Student.aspx.designer.cs
+++ Student.aspx.designer.cs
@@ -33,6 +33,15 @@ namespace Lab1
         protected global::System.Web.UI.WebControls.GridView gvStudent;
 
         /// <summary>
+        /// gvStudent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gvStudent;
+
+        /// <summary>
         /// btnAddRow control.
         /// </summary>
         /// <remarks>
