c124ce4a98aefec6d22921f2b8139df8e0604bf1
Timothy Goeke Dee <tgdee123@gmail.com>
2022-02-10 11:47:24 -0500

    Streamlining

--- Student.aspx.designer.cs
+++ Student.aspx.designer.cs
@@ -15,33 +15,6 @@ namespace Lab1
     {
 
         /// <summary>
-        /// ltError control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Literal ltError;
-
-        /// <summary>
-        /// gvStudent control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView gvStudent;
-
-        /// <summary>
-        /// btnAddRow control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnAddRow;
-
-        /// <summary>
         /// SaveButton control.
         /// </summary>
         /// <remarks>
