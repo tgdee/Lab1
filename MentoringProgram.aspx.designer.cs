d071a7df9d544ed0255793c7e9b40f2f56937c85
Shane McCarthy <mccartsf@dukes.jmu.edu>
2022-02-10 22:57:17 -0500

    mentoring program

--- MentoringProgram.aspx.designer.cs
+++ MentoringProgram.aspx.designer.cs
@@ -51,6 +51,24 @@ namespace Lab1
         protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
 
         /// <summary>
+        /// Label2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// DropDownList2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList2;
+
+        /// <summary>
         /// MentoringProgramSource control.
         /// </summary>
         /// <remarks>
@@ -58,5 +76,14 @@ namespace Lab1
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.SqlDataSource MentoringProgramSource;
+
+        /// <summary>
+        /// StudentTableDataSource control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource StudentTableDataSource;
     }
 }
