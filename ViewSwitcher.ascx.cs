f7733da37a4be5c77b398c542c9ebf97dbe8ce7f
Timothy Goeke Dee <tgdee123@gmail.com>
2022-02-19 16:39:04 -0500

    changed name

--- ViewSwitcher.ascx.cs
+++ ViewSwitcher.ascx.cs
@@ -7,7 +7,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using Microsoft.AspNet.FriendlyUrls.Resolvers;
 
-namespace Lab1
+namespace Lab2
 {
     public partial class ViewSwitcher : System.Web.UI.UserControl
     {
